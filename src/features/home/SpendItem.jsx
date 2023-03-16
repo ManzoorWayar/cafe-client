d664ff1c9de20ea3351012605ff5ebb92a2a2dda
ManzoorWayar <softwareengineerwayar@gmail.com>
2023-03-16 15:02:36 +0430

    create and fix the cash system

--- src/features/home/SpendItem.jsx
+++ src/features/home/SpendItem.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import Swal from 'sweetalert2'
+import { MdAutoDelete } from 'react-icons/md'
+import { useDeletePcMutation, } from './pcApiSlice'
 import { Col, Badge, ListGroup } from 'react-bootstrap'
+import withReactContent from 'sweetalert2-react-content'
+
+const MySwal = withReactContent(Swal)
 
 const SpendItem = ({ item, index }) => {
     const { SpendMoneyDesc, spendMoney } = item
 
+    const [delPc] = useDeletePcMutation()
+
+    const handleDelPc = () => {
+        MySwal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!',
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                await delPc(item?.id)
+            }
+        })
+    }
+
     return (
         <Col lg={6} md={6} sm={12} className='m-auto mt-5'>
             <ListGroup as="ol">
+                <MdAutoDelete title='Delete' color='red' size='20' cursor='pointer' className='me-2' onClick={handleDelPc} />
                 <ListGroup.Item
                     as="li"
                     action
@@ -17,7 +42,7 @@ const SpendItem = ({ item, index }) => {
                         <p className="fw-bold m-0 m-1">{SpendMoneyDesc}</p>
                     </div>
                     <Badge bg="info" pill>
-                        {spendMoney}
+                        {spendMoney} afg
                     </Badge>
                 </ListGroup.Item>
             </ListGroup>
